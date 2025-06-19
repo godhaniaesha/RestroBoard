cfa0668dfcf92bc6313024705a665773f3381f48
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2025-06-19 11:15:56 +0530

    szdg

---
+++ backend/middleware/auth.js
@@ -0,0 +1,69 @@
+const jwt = require("jsonwebtoken");
+const Users = require("../model/Register.model");
+ 
+const auth = (roles = []) => (req, res, next) => {
+    try {
+        // Get token from Authorization header
+        const authHeader = req.header('Authorization') || req.cookies.accessToken ;
+ 
+        console.log(authHeader,"authHeader");
+       
+        console.log('Authorization header:', authHeader);
+        if (!authHeader) {
+            return res.status(401).json({
+                success: false,
+                message: "No authorization header"
+            });
+        }
+ 
+        // Extract token from "Bearer <token>"
+        let token = authHeader;
+        console.log('Extracted token:', token);
+        if (authHeader.startsWith('Bearer ')) {
+            token = authHeader.substring(7);
+        }
+ 
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Token not available"
+            });
+        }
+ 
+        jwt.verify(token, process.env.ACCESS_TOKEN_KEY, async function (err, decoded) {
+            console.log('JWT error:', err);
+            if (err) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Token invalid"
+                });
+            }
+            console.log("decoded", decoded);
+ 
+            if (!roles.some((role) => role === decoded.role)) {
+                return res.status(400).json({
+                    success: false,
+                    message: "You have not access"
+                });
+            }
+ 
+            const USERS = await Users.findOne({ _id: decoded._id });
+            if (!USERS) {
+                return res.status(404).json({
+                    success: false,
+                    message: "User not found..!!"
+                });
+            }
+            req.user = USERS;
+            next();
+        });
+ 
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+ 
+module.exports = auth;
---
+++ backend/server.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+ 
+const express = require('express');
+// console.log("express");
+ 
+const path = require('path')
+const cookieParser = require('cookie-parser');
+const app = express();
+const cors = require('cors')
+const passport = require('passport');
+const session = require('express-session');
+ 
+const router = require('./routes');
+const connectDB = require('./db/db');
+ 
+app.use(cookieParser());
+app.use("/KAssets", express.static(path.join(__dirname, "KAssets")));
+ 
+// Session middleware configuration
+app.use(session({
+    secret: 'sdh@hehf',
+    resave: true,
+    saveUninitialized: true,
+}));
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
+ 
+const port = process.env.PORT || 2221
+ 
+ 
+connectDB()
+ 
+app.use("/", router);
+ 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+ 
