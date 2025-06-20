f4e0848ac6d36752cab95d210e2df60f93812ba1
Krupali2626 <krupalikumbhani26@gmail.com>
2025-06-20 16:58:50 +0530

    New file

---
+++ frontend/src/Container/AddEmployee.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AddEmployee(props) {
+  return (
+    <>
+      <section>
+        <h4>Employee Form</h4>
+      </section>
+    </>
+  );
+}
+
+export default AddEmployee;
